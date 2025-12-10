b3f572c903069171c5ffe19d1999d72335944910
LizandroLumbreras <lizandrodejesus0@gmail.com>
2025-12-10 11:07:49 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,5 @@
+// firebase-messaging-sw.js
+
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
@@ -5,16 +7,18 @@ firebase.initializeApp({
   apiKey: "AIzaSyCK5nb6u2CGRJ8AB1aPlRn54b97bdeAFeM",
   authDomain: "inventariopv-643f1.firebaseapp.com",
   projectId: "inventariopv-643f1",
-  storageBucket: "inventariopv-643f1.firebasestorage.app",
   messagingSenderId: "96242533231",
   appId: "1:96242533231:web:aae75a18fbaf9840529e9a"
 });
 
 const messaging = firebase.messaging();
 
+// Cuando llega una notificación en segundo plano:
 messaging.onBackgroundMessage((payload) => {
+  console.log("Background message:", payload);
+
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: payload.notification.icon
+    icon: "/logo_proveedora.webp",
   });
 });
